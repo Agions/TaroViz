fd4143082e881ec72ef059c2657f61540887bc70
Agions <1051736049@qq.com>
2026-03-16 21:06:38 +0800

    fix: 修复文档构建错误
    - 移除有问题的 playground.md
    - 移除 vitepress-plugin-tabs 依赖

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
-import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import './custom.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    enhanceAppWithTabs(app)
-  }
+  extends: DefaultTheme
 }
