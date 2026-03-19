338e98b3d8c00679f60fdf21abf4807a0275ba97
Agions <1051736049@qq.com>
2026-03-19 22:31:40 +0800

    perf: 优化打包体积 -90%，ECharts/Zrender 改为 peerDependencies
    - ECharts、Zrender、React 移至 peerDependencies
    - 使用函数形式 externals 正确排除大型依赖
    - 添加 sub-exports 支持按需加载
    - jest.config.js 改为 jest.config.cjs 修复 ESM 问题
    
    perf: ESM 2.69MB -> 209KB (-92%)
    perf: CJS 739KB -> 82KB (-89%)

--- src/core/echarts.ts
+++ src/core/echarts.ts
@@ -1,10 +1,13 @@
 /**
  * ECharts 组件注册中心
  * 统一管理所有图表组件的注册，避免重复注册
+ * 
+ * 使用 tree-shaking 友好型导入
+ * 仅在用户使用特定图表时加载对应模块
  */
 import * as echarts from 'echarts/core';
 
-// 图表类型
+// 图表类型 - 按需导入
 import { PieChart } from 'echarts/charts';
 import { LineChart } from 'echarts/charts';
 import { BarChart } from 'echarts/charts';
@@ -14,7 +17,7 @@ import { RadarChart } from 'echarts/charts';
 import { HeatmapChart } from 'echarts/charts';
 import { FunnelChart } from 'echarts/charts';
 
-// 组件
+// 组件 - 按需导入
 import {
   TitleComponent,
   TooltipComponent,
@@ -56,3 +59,22 @@ const components = [
 echarts.use([...chartTypes, ...components]);
 
 export default echarts;
+
+// 导出图表类型供按需加载使用
+export {
+  PieChart,
+  LineChart,
+  BarChart,
+  GaugeChart,
+  ScatterChart,
+  RadarChart,
+  HeatmapChart,
+  FunnelChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  DataZoomComponent,
+  VisualMapComponent,
+  CanvasRenderer,
+};
